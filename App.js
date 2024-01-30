e8c6e076f2be9687d5579d782a93858bb1a10e22
Phongsatorn <Engineer_new@hotmail.com>
2024-01-30 20:07:45 +0700

    add module chalk debug and morgan

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
-const express  = require('express');
-const chalk = require('chalk');
+import express from 'express';
+import chalk from 'chalk';
+import debug from 'debug';
+import morgan from 'morgan';
+
 const app = express();
 const port = 3000;
+
+app.use(morgan('combined'));
 
 app.get('/', (req, res) => {
     res.send('Hello, world!');
 });
 
 app.listen(port, () => {
-    console.log('listening on port' + chalk.red(port));
+     debug.log('listening on port: ' + chalk.red(port));
 })
