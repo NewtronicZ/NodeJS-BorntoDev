e8940b34ffffa67214e225aec6ee5461124d2a21
Phongsatorn <Engineer_new@hotmail.com>
2024-01-30 20:53:03 +0700

    add path

--- App.js
+++ App.js
@@ -2,11 +2,19 @@ import express from 'express';
 import chalk from 'chalk';
 import debug from 'debug';
 import morgan from 'morgan';
+import path from 'path';
 
 const app = express();
 const port = 3000;
 
+
+const pub = path.join(__dirname, 'public, index.html');
+
 app.use(morgan('combined'));
+
+app.get('/index', function (req, res) {
+    res.sendFile(path.join(pub, 'index.html'));
+});
 
 app.get('/', (req, res) => {
     res.send('Hello, world!');
