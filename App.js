bb3afc53577bb54f4c4df8b84215a2d47bb8268b
Phongsatorn <Engineer_new@hotmail.com>
2024-01-31 22:31:42 +0700

    add static web page

--- App.js
+++ App.js
@@ -2,24 +2,24 @@ import express from 'express';
 import chalk from 'chalk';
 import debug from 'debug';
 import morgan from 'morgan';
-import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path'; // Import the 'join' function from the 'path' module
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 const port = 3000;
 
-
-const pub = path.join(__dirname, 'public, index.html');
-
 app.use(morgan('combined'));
 
-app.get('/index', function (req, res) {
-    res.sendFile(path.join(pub, 'index.html'));
-});
+// Use the 'join' function to correctly build the path for static files
+app.use(express.static(join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.send('Hello, world!');
 });
 
 app.listen(port, () => {
-     debug.log('listening on port: ' + chalk.red(port));
-})
+    debug.log('listening on port: ' + chalk.red(port));
+});
